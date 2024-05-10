21f71a0bebe198bcf89663e2c2eb01680495ce52
Angel <apineiroandion@danielcastelao.org>
2024-05-10 09:40:51 +0200

    añado ver velocidad

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -1,4 +1,19 @@
 package cod.mvc;
 
+/**
+ * Clase con la interfaz y que muetra/recibe datos del usuario
+ */
 public class View {
+    //TODO : Crear un metodo que llame algun tipo de menu que interactue con el usuari
+
+    /**
+     * Metodo que muestra la matricula y la velocidad de un coche
+     * @param matricula
+     * @param velocidad
+     * @return true
+     */
+    public boolean muestraVelocidad(String matricula, Integer velocidad) {
+        System.out.println("Matricula: " + matricula + " Velocidad: " + velocidad);
+        return true;
+    }
 }
