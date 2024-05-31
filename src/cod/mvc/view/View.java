6f4f94c7ad2e0746a2dc4bbed0bd0608c534e30c
Angel <apineiroandion@danielcastelao.org>
2024-05-31 09:20:15 +0200

    creo los metodos del view, para pedir y mostrar los datos de manera grafica

--- src/cod/mvc/view/View.java
+++ src/cod/mvc/view/View.java
@@ -1,5 +1,7 @@
 package cod.mvc.view;
 
+import javax.swing.JOptionPane;
+
 /**
  * Clase con la interfaz y que muetra/recibe datos del usuario
  */
@@ -16,4 +18,26 @@ public class View {
         System.out.println("Matricula: " + matricula + " Velocidad: " + velocidad);
         return true;
     }
+
+    /**
+     * Metodo que muestra matricula, modelo y velocidad de un coche
+     * @param matricula
+     * @param modelo
+     * @param velocidad
+     * @return true
+     */
+    public static boolean muestraCoche(String matricula, String modelo, Integer velocidad) {
+        JOptionPane.showMessageDialog(null,"Matricula: " + matricula + " Modelo: " + modelo
+                + " Velocidad: " + velocidad);
+        return true;
+    }
+
+    /**
+     * Metodo que recoge la matricula del coche con un JOptionPane
+     * @return String
+     */
+    public static String getMatricula() {
+        return JOptionPane.showInputDialog("Introduce la matricula del coche");
+    }
+
 }
